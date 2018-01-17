3f284c849f1aa73df943d3efbb0eaaa4a9f9f54f
Kynan <k.song@blueyonder.co.uk>
2018-01-17 09:45:28 +0000

    Basic container set up.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import GameContainer from './GameComponents/GameContainer.jsx';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <GameContainer />
+
+    );
+  }
+}
+
+export default App;
---
+++ src/GameComponents/GameContainer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class GameContainer extends React.Component {
+
+
+  render() {
+    return (
+      <div> Hello </div>
+    )
+  }
+}
+
+export default GameContainer;
